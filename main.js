c5328d6d60c580358fe73e675d349f22bace8756
Ronald Silva <ronald.creative@gmail.com>
2021-06-25 15:02:55 -0300

    add creation of about and services sections and Js

---
+++ main.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for ( const element of toggle) {
+    element.addEventListener('click', () => {
+       nav.classList.toggle('show');
+    })
+}
+
+const links = document.querySelectorAll('nav ul li a') 
+
+
+for( const link of links ) {
+    link.addEventListener('click', () => {
+        nav.classList.remove('show');
+    })
+}
+
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+
+    } else {
+        // menor que altura do header
+        header.classList.remove('scroll')
+    }
+}) 
