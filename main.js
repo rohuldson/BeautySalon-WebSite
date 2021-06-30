ff992ecac5435fe0cc730ad0237039a2443e66d9
Ronald Silva <ronald.creative@gmail.com>
2021-06-30 00:46:41 -0300

    add footer section and animations css

--- main.js
+++ main.js
@@ -1,61 +1,78 @@
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
-for ( const element of toggle) {
-    element.addEventListener('click', () => {
-       nav.classList.toggle('show');
-    })
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
 
-const links = document.querySelectorAll('nav ul li a') 
+const links = document.querySelectorAll("nav ul li a");
 
-
-for( const link of links ) {
-    link.addEventListener('click', () => {
-        nav.classList.remove('show');
-    })
+for (const link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
 }
 
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight
-window.addEventListener('scroll', () => {
-    if(window.scrollY >= navHeight) {
-        header.classList.add('scroll')
+/*  Change the HEADER of page when scrolling */
 
-    } else {
-        // menor que altura do header
-        header.classList.remove('scroll')
-    }
-}) 
-
-
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    // menor que altura do header
+    header.classList.remove("scroll");
+  }
+}
 
 /* Testimonials carousel slider SWIPER */
 
-const swiper = new Swiper('.swiper-container', {
-   sliesPerView: 1,
-   pagination: {
-       el: '.swiper-pagination'
-   },
-   mousewheel: true,
-   keyboard: true,
-  });
+const swiper = new Swiper(".swiper-container", {
+  sliesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
+/*  scroll Reveal: mostrar elementos quando der scroll na página */
 
-  /*  scroll Reveal: mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
 
-  const scrollReveal = ScrollReveal({
-      origin: 'top',
-      distance: '30px',
-      duration: 700,
-      reset: true
-  })
-
-  scrollReveal.reveal(`
+scrollReveal.reveal(
+  `
     #home .image, #home .text, 
     #about .image, #about .text,    
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact p, #contact .button, #contact .links    
-    `, {interval: 100}
-    )
+    #contact .text, #contact p, #contact .button, #contact .links ,
+    footer .logo, footer p, footer .social
+    `,
+  { interval: 100 }
+);
+
+function backToTop() {
+    const backToTopButton = document.querySelector(".back-to-top");
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add("show");
+      } else {
+        backToTopButton.classList.remove("show");
+      }
+
+}
+
+
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop()
+});
